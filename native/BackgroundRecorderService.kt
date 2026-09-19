4f6fb736239065f3e9c5aee015d969a334af1966
Dawid Tomaszewski <dawid@iMac-Dawid.local>
2026-09-19 23:30:32 +0200

    Fix Java package path for custom BackgroundRecorder plugin

--- native/BackgroundRecorderService.kt
+++ native/BackgroundRecorderService.kt
@@ -3,7 +3,6 @@ package com.pitchrec.backgroundrecorder
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
